4d5f7ca675be889b3a62f9f1917ddbd04bcf130a
heespari <heta.e.parikka@student.jyu.fi>
2025-10-17 22:59:22 +0300

    2.5

---
+++ Course.jsx
@@ -0,0 +1,28 @@
+const Course = (props) => {
+return (
+  <div>
+    <Header course={props.course} />
+    <Content parts={props.course.parts} />
+  </div>
+)
+}
+const Header = (props) => <h2>{props.course.name}</h2>
+
+const Content = (props) => {
+  const parts = props.parts
+  return (
+    <div>
+      {parts.map(part => 
+        <Part key={part.id} part={part} />
+      )}
+    </div>
+  )
+}
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+export default Course
